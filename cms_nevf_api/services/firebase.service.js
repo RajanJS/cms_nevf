396f228475d86723e5123c52167c7dd7f1b5685d
Rajan Maharjan <rajanmaharjanmails@gmail.com>
2020-04-08 11:06:56 +0545

    deploy changes

--- cms_nevf_api/services/firebase.service.js
+++ cms_nevf_api/services/firebase.service.js
@@ -10,14 +10,14 @@ export default class firebaseService {
     });
 
     static client = firebase.initializeApp({
-        apiKey: "apikey",
-        authDomain: "authDomain",
-        databaseURL: "databaseURL",
-        projectId: "projectId",
-        storageBucket: "storageBucket",
-        messagingSenderId: "messagingSenderId",
-        appId: "appId",
-        measurementId: "measurementId"
+        apiKey: "AIzaSyBfqMmydfn1RptBzkxNAD3G0tkWi0aTUuY",
+        authDomain: "cms-nevf.firebaseapp.com",
+        databaseURL: "https://cms-nevf.firebaseio.com",
+        projectId: "cms-nevf",
+        storageBucket: "cms-nevf.appspot.com",
+        messagingSenderId: "871411917468",
+        appId: "1:871411917468:web:83c63992d07a4db81e4359",
+        measurementId: "G-0Q1KE6PFT3"
     });
 
     static db = admin.firestore();
